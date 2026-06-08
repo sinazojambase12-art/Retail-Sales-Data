d4b8e176a30fb65976c7985d85d290dfca7789eb
sinazojambase12-art <sinazojambase12@gmail.com>
2026-06-08 09:24:57 +0000

    Advanced Coding complete

--- Advanced Coding on Retail Sales.sql
+++ Advanced Coding on Retail Sales.sql
@@ -32,21 +32,49 @@ ORDER BY `Total Amount` DESC
 LIMIT 5;
 
 -- Which product categories have more than 300 transactions?
-
+SELECT `Product Category`,
+      COUNT(*) AS transact_count
+FROM retail_sales
+GROUP BY `Product Category`
+HAVING COUNT(*) > 300;
 
 --What is the highest sales amount recorded for each product category?
-
+SELECT `Product Category`,
+      MAX(`Total Amount`) AS highest_sales_amount
+FROM retail_sales
+GROUP BY `Product Category`;
 
 -- Find the minimum sale amount for each gender.
+SELECT Gender,
+       MIN(`Total Amount`) AS min_sale_amount
+FROM retail_sales
+GROUP BY Gender;
 
-
--- Show categories where the average quantity sold it greater than 2.
-
+-- Show categories where the average quantity sold is greater than 2.
+SELECT `Product Category`,
+      AVG(Quantity) AS avg_quantity_sold
+FROM retail_sales
+GROUP BY `Product Category`
+HAVING AVG(Quantity) > 2;
 
 -- Find the total sales for customers aged between 25 and 40 who purchased Beauty or Clothing products.
-
+SELECT SUM(`Total Amount`) AS total_sales
+FROM retail_sales
+WHERE Age BETWEEN 25 AND 40
+AND `Product Category` IN ('Beauty','Clothing');
 
 -- Which product categories have a maximum sale amount greater than 1000?
-
+SELECT `Product Category`,
+      MAX(`Total Amount`) AS max_sale_amount
+FROM retail_sales
+GROUP BY `Product Category`
+HAVING MAX(`Total Amount`) > 1000;
 
 -- Show the top 3 categories by average value, excluding customers under 25.
+SELECT `Product Category`,
+       AVG(`Total Amount`) AS avg_value
+FROM retail_sales
+WHERE age >= 25
+GROUP BY `Product Category`
+ORDER BY AVG(`Total Amount`) DESC
+LIMIT 3;
